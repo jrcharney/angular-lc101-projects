22bc7fa153636a2b4473a082abb5bee920c42f9d
Jason Charney <jrcharney@gmail.com>
2020-04-14 17:10:34 -0500

    Chapter 30 Exercises

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -6,11 +6,31 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent implements OnInit {
-   buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   buttonHeading: string = "Buttons";
+   //inactive: boolean = false;
+   active: boolean = true;
+   buttonState: boolean[] = [true, true, true];
+   location: string = 'center';
 
    constructor() { }
 
    ngOnInit() { }
+
+   reactiveButtons(){
+     for(let i = 0; i < this.buttonState.length; i++){
+       this.buttonState[i] = true;
+     }
+     return this.buttonState;
+   }
+
+   shiftLocation(oldLocation: string){
+     let locations : string[] = ['left','center','right'];
+     let random : number;
+     while(this.location === oldLocation){
+       random = Math.floor(Math.random() * locations.length);
+       this.location = locations[random];
+     }
+     return this.location;
+   }
 
 }
